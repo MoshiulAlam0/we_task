b2929370c846bb513d6843a4f4289e1190839bee
MoshiulAlam0 <allenshvro@gmail.com>
2024-09-27 17:04:59 +0600

    ad

--- src/Component/Head.jsx
+++ src/Component/Head.jsx
@@ -5,6 +5,7 @@ const Head = () => {
     <div className='flex items-center justify-between py-2 px-[4vmin]'>
       <h1 className='text-[1.2rem] text-white font-semibold'>We<span className='text-[#bda51b] font-light'>_</span>Todo</h1>
       <p className='text-white text-[1rem] font-light cursor-pointer hover:text-[#181C14] capitalize '>my todo</p>
+      {/* kfdsjfkjs */}
     </div>
   )
 }
